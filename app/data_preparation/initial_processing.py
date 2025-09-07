9e5992861fac835c0c30e149800b5ded52e5d23d
achiya reuven <achiya3144@gmail.com>
2025-09-07 15:14:03 +0300

    added a class that which accepts a path to a file and returns a dictionary with metadata

---
+++ app/data_preparation/initial_processing.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import os
+from tinytag import TinyTag
+import datetime
+import uuid
+
+
+class Processing:
+
+    @staticmethod
+    def meta_data_to_dict(audio_file):
+        try:
+            file_stats = audio_file.stat()
+            creation_timestamp = file_stats.st_ctime
+            dict_resulte = {}
+            if audio_file.is_file() and audio_file.suffix in [".mp3", ".wav", ".flac"]:
+                tag = TinyTag.get(str(audio_file))
+                dict_resulte["id"]=str(uuid.uuid4())
+                dict_resulte["absolute path"] = tag.filename
+                dict_resulte["name"] = audio_file.name
+                dict_resulte["creation_timestamp"] =datetime.datetime.fromtimestamp(creation_timestamp)
+                dict_resulte["size"] = file_stats.st_size
+                dict_resulte["duration"] =tag.duration
+                return dict_resulte
+            else:
+                print(f"File not found: {audio_file}")
+        except Exception as e:
+            print(f"Error extracting metadata: {e}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
