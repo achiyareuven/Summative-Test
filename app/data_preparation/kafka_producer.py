1f41ae8df45df79dd270b5d2452f396cdbdaffb3
achiya reuven <achiya3144@gmail.com>
2025-09-07 16:38:56 +0300

    added a kafka producer

---
+++ app/data_preparation/kafka_producer.py
@@ -0,0 +1,31 @@
+from kafka import KafkaProducer
+import json
+import time
+
+class Producer:
+    def __init__(self, bootstrap_servers='localhost:9092'):
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+        print(f"KafkaProducer initialized for brokers: {bootstrap_servers}")
+
+    def send_message(self, topic, value,key=None):
+
+        try:
+            future = self.producer.send(topic, key=key.encode('utf-8')if key else None, value=value)
+            record_metadata = future.get(timeout=10)
+            print(f"Message sent to topic: {record_metadata.topic}, "
+                  f"partition: {record_metadata.partition}, "
+                  f"offset: {record_metadata.offset}")
+        except Exception as e:
+            print(f"Error sending message: {e}")
+
+    def flush_producer(self):
+        self.producer.flush()
+        self.producer.close()
+
+
+
+
+
