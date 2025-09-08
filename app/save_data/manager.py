a0d4d5f9b66d1419c2a88d8bdf90d8453214501e
achiya reuven <achiya3144@gmail.com>
2025-09-08 12:15:49 +0300

    I added a manager that reads messages from the consumer, writes to Mongo and Elastic, and commits.

--- app/save_data/manager.py
+++ app/save_data/manager.py
@@ -1,21 +1,55 @@
+import os
 from app.dal.es_client import Elastic
+from app.dal.mongo_dal import MongoDAL
 from app.save_data.kafka_consumer import Consumer
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_URL=os.getenv("MONGO_URL", "mongodb://localhost:27017/")
+MONGO_DB = os.getenv("MONGO_DB", "appdb")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", "docs")
+
+ES_URI = os.getenv("ES_URI","http://localhost:9200")
+ES_INDEX = os.getenv("ES_INDEX","audio_docs")
+
+KAFKA_TOPIC =os.getenv("KAFKA_TOPIC","audio_meta")
+KAFKA_GROUP = os.getenv("KAFKA_GROUP","read_meta")
 
 
 
 class Manager:
-    def __init__(self, elastic: Elastic,mapping:dict=None,):
+    def __init__(self, elastic: Elastic):
         self.elastic  = elastic
-        self.consumer = Consumer("jjj","dds")
+        self.consumer = Consumer(KAFKA_TOPIC,KAFKA_GROUP)
+        self.mongo= MongoDAL(MONGO_URL,MONGO_DB,MONGO_COLLECTION)
 
-    def reset_index(self):
-        self.elastic.create_index()
+    def run(self):
+        try:
+            for msg, commit in self.consumer.get_consumer_events():
+                if not msg["id"] and not msg["absolute_path"]:
+                    print(f"No path or id found Skip message{msg}")
+                    continue
+                self.mongo.insert_audio_file(msg["absolute_path"],msg["id"])
 
-    def index_doc(self, doc: dict, doc_id=None):
-        return self.elastic.es.index(index=self.elastic.index_name, id=doc_id, document=doc)
+                self.elastic.index_doc(msg,msg["id"])
+                print("save ela")
+
+                commit()
+        except Exception as e:
+            print(f"error save data {e}")
+
+if __name__ == "__main__":
+    elastic = Elastic(ES_URI, ES_INDEX)
+    manager = Manager(elastic)
+    manager.run()
 
 
-    def
+
+
+
+
+
+
 
 
 
