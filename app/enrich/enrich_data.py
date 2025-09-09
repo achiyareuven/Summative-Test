e5776f2babec1a216c5736d9dfa66846a66b5e22
achiya reuven <achiya3144@gmail.com>
2025-09-09 17:59:56 +0300

    added class

---
+++ app/enrich/enrich_data.py
@@ -0,0 +1,19 @@
+
+
+
+class TextProcessor:
+    def __init__(self,list_hostile, less_hostile):
+        self.list_hostile = list_hostile
+        self.less_hostile =less_hostile
+
+    def check_include_bds(self):
+        pass
+
+    def is_bds(self):
+        pass
+
+    def percent_bds(self):
+        pass
+
+    def bds_level(self):
+        pass
