dadd671cf318806e181524ef9ec06dd043f843cf
achiya reuven <achiya3144@gmail.com>
2025-09-09 12:32:14 +0300

    added function reading the audio file

--- app/dal/mongo_dal.py
+++ app/dal/mongo_dal.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from gridfs import GridFS
 from app.logger import Logger
 import os
-
+import tempfile
 logger = Logger.get_logger()
 
 class MongoDAL:
@@ -24,6 +24,22 @@ class MongoDAL:
             logger.error(f"Error: Audio file not found at {audio_file_path}")
         except Exception as e:
             logger.error(f"An error occurred: {e}")
+
+    def get_audio_file(self,id_file,file_name):
+        try:
+            file_data = self.fs.find_one({"_id": id_file})
+            if file_data:
+                with open(rf'..\{file_name}', 'wb') as output_file:
+                    output_file.write(file_data.read())
+                logger.info("File downloaded successfully")
+                return output_file.name
+            else:
+                logger.info("File not found")
+        except FileNotFoundError:
+            logger.error(f"File not found with id {id_file}")
+        except Exception as e:
+            logger.error(f"error to get file from mongo with id {id_file}")
+
 
     def close(self):
         self.client.close()
