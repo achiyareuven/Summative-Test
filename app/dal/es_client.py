d612b75ab5940e3a4ecf8dec15cfb7a9636dd28d
achiya reuven <achiya3144@gmail.com>
2025-09-09 12:35:40 +0300

    added functions to update fild and func to check if fild already exists

--- app/dal/es_client.py
+++ app/dal/es_client.py
@@ -31,6 +31,25 @@ class Elastic:
         except Exception as e:
             logger.error(f"Indexing doc failed: {e}")
 
+    def check_field_exists_in_document(self, doc_id: str, field_name: str) -> bool:
+        try:
+            logger.info("Checking if a field exists in doc")
+            response = self.es.get(index=self.index_name, id=doc_id, _source_includes=[field_name])
+            if response.get('found') and field_name in response['_source']:
+                return True
+            else:
+                return False
+        except Exception as e:
+            logger.error(f"Error checking field existence: {e}")
+            return False
+
+    def update_doc(self, doc_id: str, new_values: dict):
+        try:
+            result = self.es.update(index=self.index_name, id=doc_id, body={"doc": new_values})
+            logger.info(f"Updated doc id with{doc_id}")
+            return result
+        except Exception as e:
+            logger.error(f"Update failed: {e}")
 
 
     def delete_documents_by_id(self, ids: list):
